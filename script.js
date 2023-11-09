e50be35c1fbbdb9c497b9c0fba9236290824b0f6
SbnBrmr <sobermei@web.de>
2023-11-09 16:32:20 +0100

    test

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var greetingElement = document.querySelector('h1');
+    greetingElement.textContent = 'Hallo von TypeScript!';
+});
---
+++ script.ts
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const greetingElement = document.querySelector('h1');
+    greetingElement.textContent = 'Hallo von TypeScript!';
+});
